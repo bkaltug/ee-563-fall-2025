f742c470c6a892df653ac4a3e27ff948885369ca
bkaltug <berkay.ustagul@gmail.com>
2025-11-11 15:53:56 +0300

    Pose detector works as requested.

--- midterm/pose_detection.py
+++ midterm/pose_detection.py
@@ -32,10 +32,46 @@ options = vision.PoseLandmarkerOptions(
     base_options=base_options,
     output_segmentation_masks=True)
 detector = vision.PoseLandmarker.create_from_options(options)
-
 image = mp.Image.create_from_file(user_choice)
-
 detection_result = detector.detect(image)
+
+pose_landmarks_list = detection_result.pose_landmarks
+arm_status = "None"
+VISIBILITY_THRESHOLD = 0.5 
+
+if pose_landmarks_list:
+    landmarks = pose_landmarks_list[0]
+
+    left_arm_up = False
+    right_arm_up = False
+
+    left_shoulder = landmarks[11]
+    right_shoulder = landmarks[12]
+    left_wrist = landmarks[15]
+    right_wrist = landmarks[16]
+
+
+    if left_wrist.visibility > VISIBILITY_THRESHOLD and left_shoulder.visibility > VISIBILITY_THRESHOLD:
+
+        if left_wrist.y < left_shoulder.y:
+            left_arm_up = True
+            
+
+    if right_wrist.visibility > VISIBILITY_THRESHOLD and right_shoulder.visibility > VISIBILITY_THRESHOLD:
+  
+        if right_wrist.y < right_shoulder.y:
+            right_arm_up = True
+
+    if left_arm_up and right_arm_up:
+        arm_status = "both"
+    elif left_arm_up:
+        arm_status = "left"
+    elif right_arm_up:
+        arm_status = "right"
+    else:
+        arm_status = "None"
+
+print(arm_status)
 
 annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
 cv2.imshow('Pose Result',cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
